7a39dddabf88408ec2135b6d3f2a5aaf8cc163f3
LucasJWilliams <67558139+LucasJWilliams@users.noreply.github.com>
2021-09-06 16:21:35 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import discord
+from discord_slash import SlashCommand, SlashContext
 from discord.ext import commands
 
 from config import discord_secret, extensions, prefix
@@ -7,6 +8,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 bot = commands.Bot(command_prefix=prefix, description="Revision server bot", intents=intents, case_insensitive=True)
+slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True)
 
 
 @bot.event 
@@ -14,12 +16,14 @@ async def on_ready():
     print("--------")
     print(f"Logged in as {bot.user.name} - {bot.user.id}")
     print("--------")
-
+    
+        
     print("Loading cogs...")
     for file in extensions:
         bot.load_extension(f"cogs.{file}")
         print(f"Loaded cogs.{file}")
     print("--------")
 
+bot.load_extension("cogs.modmail")
 
 bot.run(discord_secret)
