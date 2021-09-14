b82c59a4b555c7026b5aadd0b2d2dc2d99d729ea
LucasJWilliams <67558139+LucasJWilliams@users.noreply.github.com>
2021-09-14 22:43:22 +0100

    Update misc.py

--- src/cogs/misc.py
+++ src/cogs/misc.py
@@ -1,4 +1,4 @@
-import discord
+import discord, asyncio
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_permission
@@ -11,8 +11,26 @@ class Misc(commands.Cog, name="Miscellaneous commands"):
   
     def __init__(self, bot):
         self.bot = bot
-        
+  
 
+    @commands.command(name="ban")
+    @commands.has_role(829495730464882741) #Staff on modmail server
+    async def ban(self, ctx):
+        messages = []
+        for i in range(4):
+            message = await ctx.send("ban")
+            messages.append(message)
+        
+        await asyncio.sleep(1)
+        
+        for message in messages:
+            await message.delete()
+
+
+    @cog_ext.cog_slash(name="ping", description="Gets the bots ping", guild_ids=modmail_guild_id)
+    async def ping(self, ctx:SlashContext):
+        await ctx.reply(f'Pong! In {round(self.bot.latency * 1000)}ms')
+	
     @cog_ext.cog_slash(name="ping", description="Gets the bots ping", guild_ids=modmail_guild_id)
     async def ping(self, ctx:SlashContext):
         await ctx.reply(f'Pong! In {round(self.bot.latency * 1000)}ms')
